620f8322dd54b531dc0df9af3eda39c10ada25fa
Manuel Caicedo <manuelcaicedo52@gmail.com>
2023-03-30 22:01:17 -0500

    created pojo, entity and mapper of  Marca Coche

---
+++ src/main/java/com/project/coches/persistance/entity/MarcaCocheEntity.java
@@ -0,0 +1,30 @@
+package com.project.coches.persistance.entity;
+
+import jakarta.persistence.*;
+import lombok.Getter;
+import lombok.Setter;
+
+/**
+ * entidad de marca coche
+ */
+@Getter
+@Setter
+@Entity
+@Table(name="marca_choche")
+public class MarcaCocheEntity {
+
+
+    /**
+     * id de la marca
+     */
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Integer id;
+
+
+    /**
+     * descripcion de la marca
+     */
+    @Column(name="description")
+    private String description;
+}
---
+++ src/main/java/com/project/coches/persistance/mapper/IMarcaCocheMapper.java
@@ -0,0 +1,40 @@
+package com.project.coches.persistance.mapper;
+
+import com.project.coches.domain.pojo.MarcaCochePojo;
+import com.project.coches.persistance.entity.MarcaCocheEntity;
+import org.mapstruct.InheritInverseConfiguration;
+import org.mapstruct.Mapper;
+import org.mapstruct.Mapping;
+
+import java.util.List;
+
+/**
+ * Mapper que transforma objetos de MarcaCoche a pojos o entidades
+ */
+@Mapper(componentModel = "spring")
+public interface IMarcaCocheMapper {
+
+    /**
+     * Convierte una entidad a un pojo de marca coche
+     * @param marcaEntity Entidad a convertir
+     * @return Pojo convertido
+     */
+    @Mapping(source = "id", target = "id")
+    @Mapping(source = "description", target = "description")
+    MarcaCochePojo toMarcaCochePojo(MarcaCocheEntity marcaEntity);
+
+    /**
+     * Convierte una entidad a un pojo de marca coche
+     * @param marcaPojo Pojo a convertir
+     * @return Entidad convertida
+     */
+    @InheritInverseConfiguration
+    MarcaCocheEntity toMarcaCocheEntity(MarcaCochePojo marcaPojo);
+
+    /**
+     * Retorna una lista de marcas coche transformada a pojo de una lista de entidades
+     * @param marcasCocheEntity Entidad a transformar
+     * @return Lista transdormada
+     */
+    List<MarcaCochePojo> toMarcasCochePojo(List<MarcaCocheEntity> marcasCocheEntity);
+}
